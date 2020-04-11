52e9f3cf8896374f5fb809754c9c748d88f7624f
Baptiste SUZON <baptiste.suzon@epita.fr>
2020-04-11 05:48:43 +0200

    Consistency fix in pillager decision tree

--- src/roles/roles.pillager.ts
+++ src/roles/roles.pillager.ts
@@ -1,7 +1,7 @@
 import { goPillage, goStoreResource } from "./utils";
 
 export function run(creep: Creep) {
-  if (creep.store.getFreeCapacity() === 0) {
+  if (!goPillage(creep)) {
     goStoreResource(creep, RESOURCE_ENERGY);
   } else {
     goPillage(creep);
